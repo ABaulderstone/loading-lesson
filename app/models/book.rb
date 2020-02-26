28413f156f08cf54271d4e5981f6c4b05b48a635
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-02-26 16:12:32 +1100

    tided up query

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,15 +2,15 @@ class Book < ApplicationRecord
   belongs_to :author
 
   def self.search(query)
-    if query
-      author = Author.find_by(name: query)
+    if query !="" && query !=nil
+      author = Author.where('name LIKE ?', "%#{query}%").first
       if author 
         return self.where(author_id: author.id).includes(:author).references(:author)
       else 
-        return self.all
+        return self.all.includes(:author).references(:author)
       end
     else 
-      return self.all
+      return self.all.includes(:author).references(:author)
     end
   end 
 end
